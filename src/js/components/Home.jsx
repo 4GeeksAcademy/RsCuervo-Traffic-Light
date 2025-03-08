687a3bcd32f666d7323ff280b6e7248717ce80ee
Richard Cuervo <richardcuer@hotmail.com>
2025-03-08 00:26:58 +0000

    [Semáforo con luces que brillan al activarlas] Se crearon clases y se modifico css. Se importo useState y se aplico en React.

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState(null);
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+        <div className="stick"></div>
+		<div className="trafficLight">
+            <div className={`lightRed ${color == "Red" ? "glow" : null}`}
+				onClick={() => {
+                    setColor("Red")
+				}}
+			></div>
+			<div className={`lightYellow ${color == "Yellow" ? "glow" : null}`}
+			onClick={() => {
+				setColor("Yellow")
+			}}
+			></div>
+			<div className={`lightGreen ${color == "Green" ? "glow" : null}`}
+			onClick={() => {
+				setColor("Green")
+			}}
+			></div>
 		</div>
+		</>
 	);
 };
 
---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrafficLight = () => {
+  return (
+    <div>TrafficLight</div>
+  )
+}
+
+export default TrafficLight
